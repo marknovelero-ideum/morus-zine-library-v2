c9c838cc7a2817fb6277bf21d81233e3991eb2df
Mark Novelero <97040015+marknovelero-ideum@users.noreply.github.com>
2024-05-05 18:41:18 -0400

    Update: First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='header'>
+        <h1 className='title'>M.O.R.U.S</h1>
+        <h2 className='subtitle'>Zine Library</h2>
+    </header>
+  )
+}
