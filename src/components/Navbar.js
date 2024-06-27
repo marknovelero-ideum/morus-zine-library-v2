383c0394b30661033176a3d039641f0b8c47cde8
Mark Novelero <97040015+marknovelero-ideum@users.noreply.github.com>
2024-06-27 14:58:01 -0400

    Update about page.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,23 +9,25 @@ export default function Navbar( {current, setCurrent} ) {
   }
   
   return (
-    <nav className="navbar">
-    <div className="left">
-      <h1 className="main-title">M.O.R.U.S</h1>
-     
-    </div>
-    <div className="right">
-      <ul className='nav-container'>
-        <li >
-          <p className = {current === "about" ? "nav-link current" : "nav-link"} onClick={() => handleCurrentChange('about')}>About</p>
-        </li>
-        <li>
-          <p className = {current === "zine" ? "nav-link current" : "nav-link"} onClick={() => handleCurrentChange('zine')}>Zine Collection</p>
-        </li>
-        <li>
-          <p className = {current === "submit" ? "nav-link current" : "nav-link"} onClick={() => handleCurrentChange('submit')} >Submissions</p>
-        </li>
-      </ul>
+    <nav className="top-nav">
+      <div className="navbar-container">
+        <div className="left">
+          <h1 className="main-title">M.O.R.U.S</h1> 
+          <h2 className='subtitle'>Zine Library</h2>
+        </div>
+        <div className="right">
+          <ul className='nav-container'>
+            <li >
+              <p className = {current === "about" ? "nav-link current" : "nav-link"} onClick={() => handleCurrentChange('about')}>About</p>
+            </li>
+            <li>
+              <p className = {current === "zine" ? "nav-link current" : "nav-link"} onClick={() => handleCurrentChange('zine')}>Zine Collection</p>
+            </li>
+            <li>
+              <p className = {current === "submit" ? "nav-link current" : "nav-link"} onClick={() => handleCurrentChange('submit')} >Submissions</p>
+            </li>
+          </ul>
+      </div>
     </div>
   </nav>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,44 @@
 import React, { useState } from 'react'
 import Navbar from '../components/Navbar.js'
+import Morus from '../assets/fonts/morus.png'
+import PointR from '../assets/fonts/pointing.png'
 import './home.css'
 
 export default function Home(zineData) {
   const [current, setCurrent] = useState('about')
 
   return (
-    <div>
+    <main>
       <Navbar current={current} setCurrent={setCurrent}></Navbar>
-      <div className='about-container'>
-       
-      </div>
-    </div>
+      <article className="about-top">
+        <container className="para-1-container">
+          <section className="para-1">
+            <div className='container-label-1'>
+              <div className='container-label-sub'>
+                <p className='para1-label'>Paragraph 1 -----</p>
+                <img className='img-lable-1' src={PointR}></img>
+              </div>
+            </div>
+            <p className="para-1-text">
+            The Museum of Reclaimed Urban Space Zine Library was created to support the museum’s mission of preserving and
+             promoting the history of grassroots activism in the Lower East Side of Manhattan. This library elevates and 
+             makes accessible the oftentimes disenfranchised and marginalized voices represented through these typically 
+             free publications. First organized in 2021 by the Archive Committee at MoRUS, the MoRUS Zine Library was made 
+             up of donations from visitors and former museum volunteers. Moving forward the scope of our zine collection 
+             policy will be focused on subjects and genres ranging from DIY, history, and art that deal with topics like 
+             housing, environmentalism, and activism, especially those emanating from New York City. Despite these 
+             collection restrictions, the library will continue to reflect the diverse communities of stakeholders concerned
+             with each of these topics. 
+            </p>
+          </section>
+        </container>
+        <div className="deco-div-1"></div>
+        <div className='img-1-container'>
+              <img className='img-1' src={Morus}></img>
+              <div className='deco-div-2'></div>
+        </div>
+      </article>
+     
+    </main>
   )
 }
